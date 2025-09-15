aa5946da406e8c5c50ed455ef84feb5e96614aef
Martin Silva <Martin.Silva01@student.csulb.edu>
2025-09-15 14:32:32 -0700

    added example_graph.gml, added filename validation

--- graph.py
+++ graph.py
@@ -1,14 +1,25 @@
 import argparse
 import networkx as nx
+import re
+
+def validFileName(str):
+    file_name_array = str.split('.')
+    if len(file_name_array) != 2:
+        raise argparse.ArgumentTypeError('File name requires exactly one .gml extension.')
+    if file_name_array[1] != "gml":
+        raise argparse.ArgumentTypeError('File name must end in .gml.')
+    if re.search(r'[^A-Za-z\d_-]', file_name_array[0]):
+        raise argparse.ArgumentTypeError('File name must not contain non-alphanumeric characters.')
 
 parser = argparse.ArgumentParser(description='Python application that handles Erdős–Rényi random graph generation, analysis, transformation, and visualization.', formatter_class=argparse.RawTextHelpFormatter)
 
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("--input-file", help='Reads a graph from the given .gml file and uses it for all subsequent operations.')
-group.add_argument("--create-random-graph", nargs=2, metavar=('n', 'c'), help='Generates a new Erdős–Rényi graph with n nodes and edge probability p=(c⋅ln(n)/n). Overrides --input. Nodes must be labeled with strings ("0", "1", ..., "n-1").')
+group_input = parser.add_mutually_exclusive_group(required=True)
+group_input.add_argument("--input-file", type=validFileName, help='Reads a graph from the given .gml file and uses it for all subsequent operations.')
+group_input.add_argument("--create-random-graph", nargs=2, metavar=('n', 'c'), help='Generates a new Erdős–Rényi graph with n nodes and edge probability p=(c⋅ln(n)/n). Overrides --input. Nodes must be labeled with strings ("0", "1", ..., "n-1").')
 
-parser.add_argument("--multi_BFS", nargs='+', metavar=("a1"), help='Accepts one or more starting nodes and computes BFS trees from each, storing all shortest paths. Each BFS tree must be independently visualized and compared.')
-parser.add_argument("--analyze", action="store_true", 
+group_action = parser.add_mutually_exclusive_group(required=True)
+group_action.add_argument("--multi_BFS", nargs='+', metavar=("a1"), help='Accepts one or more starting nodes and computes BFS trees from each, storing all shortest paths. Each BFS tree must be independently visualized and compared.')
+group_action.add_argument("--analyze", action="store_true", 
                     help='''Performs additional structural analyses on the graph, including:
 
     Connected Components
@@ -28,7 +39,7 @@ parser.add_argument("--analyze", action="store_true",
 
     Average Shortest Path Length
     If the graph is connected, computes the average number of steps along the shortest paths for all pairs of nodes.''')
-parser.add_argument("--plot", action="store_true", 
+group_action.add_argument("--plot", action="store_true", 
                     help='''Visualizes the graph with:
 
     Highlighted shortest paths from each BFS root node;
@@ -36,7 +47,9 @@ parser.add_argument("--plot", action="store_true",
     Distinct styling for isolated nodes;
     
     Optional visualization of individual connected components.''')
-parser.add_argument("--output-file", help='Saves the final graph, with all computed attributes (e.g., distances, parent nodes, component IDs), to the specified .gml file')
+parser.add_argument("--output-file", type=validFileName, default="output_graph.gml", help='Saves the final graph, with all computed attributes (e.g., distances, parent nodes, component IDs), to the specified .gml file')
+
+    
 
 args = parser.parse_args()
 
@@ -52,3 +65,7 @@ elif args.create_random_graph:
 
 else:
     raise argparse.ArgumentTypeError('Input graph required from file or random graph.')
+
+
+
+

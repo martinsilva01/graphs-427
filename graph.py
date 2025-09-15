7922367c1be301b9ad37aa31b621736e3980c9a1
Martin Silva <Martin.Silva01@student.csulb.edu>
2025-09-15 02:30:53 -0700

    added read function from nx to --input-file

--- graph.py
+++ graph.py
@@ -1,14 +1,15 @@
 import argparse
+import networkx as nx
 
-parser = argparse.ArgumentParser(
-        description='Python application that handles Erdős–Rényi random graph generation, analysis, transformation, and visualization.',
-        formatter_class=argparse.RawTextHelpFormatter
-        )
+parser = argparse.ArgumentParser(description='Python application that handles Erdős–Rényi random graph generation, analysis, transformation, and visualization.', formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument("--input-file", help='Reads a graph from the given .gml file and uses it for all subsequent operations.')
-parser.add_argument("--create-random-graph", nargs=2, metavar=('n', 'c'), help='Generates a new Erdős–Rényi graph with n nodes and edge probability p=(c⋅ln(n)/n). Overrides --input. Nodes must be labeled with strings ("0", "1", ..., "n-1").')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("--input-file", help='Reads a graph from the given .gml file and uses it for all subsequent operations.')
+group.add_argument("--create-random-graph", nargs=2, metavar=('n', 'c'), help='Generates a new Erdős–Rényi graph with n nodes and edge probability p=(c⋅ln(n)/n). Overrides --input. Nodes must be labeled with strings ("0", "1", ..., "n-1").')
+
 parser.add_argument("--multi_BFS", nargs='+', metavar=("a1"), help='Accepts one or more starting nodes and computes BFS trees from each, storing all shortest paths. Each BFS tree must be independently visualized and compared.')
-parser.add_argument("--analyze", action="store_true", help='''Performs additional structural analyses on the graph, including:
+parser.add_argument("--analyze", action="store_true", 
+                    help='''Performs additional structural analyses on the graph, including:
 
     Connected Components
     Counts how many distinct connected subgraphs exist.
@@ -27,7 +28,7 @@ parser.add_argument("--analyze", action="store_true", help='''Performs additiona
 
     Average Shortest Path Length
     If the graph is connected, computes the average number of steps along the shortest paths for all pairs of nodes.''')
-parser.add_argument("--plot", 
+parser.add_argument("--plot", action="store_true", 
                     help='''Visualizes the graph with:
 
     Highlighted shortest paths from each BFS root node;
@@ -37,5 +38,17 @@ parser.add_argument("--plot",
     Optional visualization of individual connected components.''')
 parser.add_argument("--output-file", help='Saves the final graph, with all computed attributes (e.g., distances, parent nodes, component IDs), to the specified .gml file')
 
-
 args = parser.parse_args()
+
+G = None
+
+if args.input_file:
+    G = nx.read_gml(args.input_file)
+    print(G.nodes)
+    print(G.edges)
+
+elif args.create_random_graph:
+    print('add functionality')
+
+else:
+    raise argparse.ArgumentTypeError('Input graph required from file or random graph.')
